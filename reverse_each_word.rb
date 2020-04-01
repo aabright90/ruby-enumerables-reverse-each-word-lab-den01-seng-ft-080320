f513aa3cd31e8c02680fe8941a99f8a83cd56359
Austen Bright <pureaustenbright@gmail.com>
2020-04-01 01:41:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
-
-string.split(" ").each do |char|
- return char.collect.reverse.join(" ")
+  arr = []
+string.split(" ").collect do |char|
+ char.collect.reverse
   end
+  arr.join(" ")
 end
